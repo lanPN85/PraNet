d21098e12d41de9479fb64fb1b109c5ce6fa8e48
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2021-10-11 19:51:56 +0700

    Update MyTest.py

--- MyTest.py
+++ MyTest.py
@@ -27,6 +27,7 @@ test_loader = test_dataset(image_root, gt_root, opt.testsize)
 
 for i in range(test_loader.size):
     image, gt, name = test_loader.load_data()
+    print(name)
     gt = np.asarray(gt, np.float32)
     gt /= (gt.max() + 1e-8)
     image = image.cuda()
@@ -36,4 +37,4 @@ for i in range(test_loader.size):
     res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
     res = res.sigmoid().data.cpu().numpy().squeeze()
     res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-    imageio.imwrite(save_path+name, res)
+    imageio.imwrite(os.path.join(save_path, name), res)
